86f1d2b6bb59595820858eae00025e0e6f40ee09
Teddy26-rzky <rizkyteddyaimarr@gmail.com>
2025-04-29 01:45:37 +0700

    Add bales-room/config.php 1745865936586

---
+++ bales-room/config.php
@@ -0,0 +1,21 @@
+<?php
+// config.php - konfigurasi koneksi database
+
+$host = 'localhost';
+$db   = 'bales_room';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
